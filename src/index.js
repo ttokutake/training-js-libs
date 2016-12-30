26b14b9702cd250032b08b5dc8161b0c14855c89
aTadatoshiTokutake <tadatoshi.tokutake@access-company.com>
2016-12-30 15:50:06 +0900

    Add example for rest parameters

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ console.log('### import and export ###');
 import {ONE, TWO} from './constant';
 console.log(ONE, TWO);
 
+console.log('### Rest Parameters ###');
+function func1(...args) {
+  args.forEach(arg => {
+    console.log(arg);
+  });
+}
+func1([1, 2, 3]);
+
 console.log('### Object rest spread ###');
 const obj1 = {a: 1, b: 2, c: 3};
 const obj2 = {c: 4, d: 5};
