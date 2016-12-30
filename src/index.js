6bb70c859cfb3d6d2d800ec7c15dae13f67f769f
aTadatoshiTokutake <tadatoshi.tokutake@access-company.com>
2016-12-30 14:30:08 +0900

    Add "string.js"

--- src/index.js
+++ src/index.js
@@ -47,6 +47,10 @@ import humps from 'humps';
 console.log(humps.camelize('one_two_three'));
 console.log(humps.decamelize('OneTwoThree'));
 
+console.log('### string ###');
+import S from 'string';
+console.log(S('<p>hello, world!</p>').between('<p>', '</p>').s);
+
 
 console.log('### isomorphic-fetch ###');
 import 'isomorphic-fetch';
