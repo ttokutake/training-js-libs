ed5a37f0d86dad6998e6c247b3afb319e7bd949d
aTadatoshiTokutake <tadatoshi.tokutake@access-company.com>
2017-01-24 20:11:47 +0900

    yarn add --dev immutable

--- src/collection.js
+++ src/collection.js
@@ -23,3 +23,9 @@ console.log('### Ramda ###');
 import R from 'Ramda';
 const flatMapped = R.chain(n => [n, n])(R.range(1, 10));
 console.log(flatMapped);
+
+console.log('### Immutable ###');
+import Immutable from 'immutable';
+const obj_im = Immutable.fromJS({a: 1, b: 2, c: [3, 4, 5]});
+console.log(obj_im);
+console.log(obj_im.toJS());
