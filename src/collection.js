d53fad7761a34a918f5eb98cb716ad8eea329865
aTadatoshiTokutake <tadatoshi.tokutake@access-company.com>
2017-04-28 09:37:35 +0900

    Remove lodash and icepick

--- src/collection.js
+++ src/collection.js
@@ -1,24 +1,3 @@
-console.log('### Lodash ###');
-import _ from 'lodash';
-const indices = _.range(10);
-_.each(indices, (v, _k) => {
-  console.log(v);
-});
-
-console.log('### icepick ###');
-import i from 'icepick';
-const obj = {
-  a: 1,
-  b: {
-    c: 2,
-    d: [3, 4],
-  },
-}
-console.log(obj);
-const new_obj = i.updateIn(obj, ['b', 'd', 1], v => v * v);
-console.log(obj);
-console.log(new_obj);
-
 console.log('### Lazy ###');
 import Lazy from 'lazy.js';
 console.log(Lazy({a: 1, b: 2}).assign({c: 3}));
