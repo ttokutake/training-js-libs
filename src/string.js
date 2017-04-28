30dbbf87f4b941630c4146bce835093cfd35c05e
aTadatoshiTokutake <tadatoshi.tokutake@access-company.com>
2017-04-28 10:05:41 +0900

    Add escape-string-regexp

--- src/string.js
+++ src/string.js
@@ -6,3 +6,7 @@ console.log(humps.decamelize('OneTwoThree'));
 console.log('### string ###');
 import S from 'string';
 console.log(S('<p>hello, world!</p>').between('<p>', '</p>').s);
+
+console.log('### escape-string-regexp ###');
+import escapeRegExp from 'escape-string-regexp';
+console.log(escapeRegExp('[* + *]'));
