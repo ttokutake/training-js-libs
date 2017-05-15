63b26091c4112c7565feb5900b08352f7d2999e5
aTadatoshiTokutake <tadatoshi.tokutake@access-company.com>
2017-05-15 10:02:15 +0900

    Add jStat and prob.js

--- src/math.js
+++ src/math.js
@@ -6,4 +6,13 @@ console.log('/* math.js */');
 import math from 'mathjs';
 console.log(math.randomInt(0, 10));
 
+console.log('/* jStat */');
+import {jStat} from 'jStat';
+console.log([0, 0, 0].map(() => jStat.normal.sample(50, 1.0)));
+
+console.log('/* prob.js */');
+import Prob from 'prob.js';
+const r = Prob.normal(50, 1.0);
+console.log([0, 0, 0].map(() => r()));
+
 console.log();
